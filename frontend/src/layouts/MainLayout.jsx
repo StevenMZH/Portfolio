126f50cbde19cf6286c5f7e9c4bd9cf6b75a40a4
StevenMZH <121063564+StevenMZH@users.noreply.github.com>
2026-01-09 19:45:02 -0600

    Fix: Delete tap box on Mobile

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,24 +1,37 @@
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Background from "./Background";
 
 export default function Layout() {
+  const [deviceClass, setDeviceClass] = useState(
+    window.innerWidth <= 768 ? "mobile" : "desktop"
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDeviceClass(window.innerWidth <= 768 ? "mobile" : "desktop");
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <Background/>
+      <Background />
 
-      <div className="layout full-view column gap30">
+      <div className={`layout full-view column gap30 ${deviceClass}`}>
         <header>
           <Header />
         </header>
-        
 
         <main id="app-main" className="full-view flex column">
           <Outlet />
         </main>
 
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
