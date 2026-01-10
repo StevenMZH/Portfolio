c9ac6af1865c85d150eccdabeff530e0ebb9cb8e
StevenMZH <121063564+StevenMZH@users.noreply.github.com>
2026-01-09 23:44:45 -0600

    Feat: Update cv

--- frontend/src/components/header/CV.jsx
+++ frontend/src/components/header/CV.jsx
@@ -1,6 +1,6 @@
 export function CV() {
   return (
-    <a className="download-cv" href="StevenMendoza_CV.pdf" download>
+    <a className="download-cv" href="Steven_Mendoza_Software_Engineer.pdf" download>
       <p>CV</p>
       <img src="icons/download.svg" alt="download cv" />
     </a>
